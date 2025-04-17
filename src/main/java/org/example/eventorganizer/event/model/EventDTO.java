fdc33b0fa0600243db6bab5e72e446c5cd08678a
Luigi Monteforte <monteforteluigi28@gmail.com>
2025-04-17 12:28:19 +0200

    ✨ (feat): Add organizer and attendee lists to EventDTO

--- src/main/java/org/example/eventorganizer/event/model/EventDTO.java
+++ src/main/java/org/example/eventorganizer/event/model/EventDTO.java
@@ -1,23 +1,30 @@
 package org.example.eventorganizer.event.model;
 
-import lombok.Value;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.example.eventorganizer.event.enums.EventStatus;
 import org.example.eventorganizer.event.enums.EventType;
 
-import java.io.Serializable;
-import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Value;
 
 /**
  * DTO for {@link Event}
  */
 @Value
+@Builder
 public class EventDTO implements Serializable {
-    String title;
-    String description;
-    String location;
-    LocalDateTime startingDate;
-    LocalDateTime endingDate;
-    EventType eventType;
-    EventStatus eventStatus;
-    Integer availableSeats;
+	String title;
+	String description;
+	String location;
+	LocalDateTime startingDate;
+	LocalDateTime endingDate;
+	EventType eventType;
+	EventStatus eventStatus;
+	Integer availableSeats;
+
+	List<Long> organizerIds;
+	List<Long> attendeeIds;
 }
